493450cd470ba5bee2dc0b8426c402296132d9b8
jessica <e1669056@u.nus.edu>
2026-08-20 15:08:58 +0800

    Complete Level-1

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 /**
  * Starts the Mochi chatbot application.
  */
+
 public class Mochi {
     public static void main(String[] args) {
         String banner = " __  __            _     _ \n"
@@ -15,7 +18,20 @@ public class Mochi {
         System.out.println("Hello! I'm Mochi.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String command = scanner.nextLine(); // creates string so use .equals
+            System.out.println(separator);
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
