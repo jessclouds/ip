1c9dd964ad9a56a2c075f8ff74ad583710b64b0c
jessica <e1669056@u.nus.edu>
2026-08-20 16:31:17 +0800

    Complete Level-3

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
  * Starts the Mochi chatbot application.
  */
 
+
 public class Mochi {
     public static void main(String[] args) {
         String banner = " __  __            _     _ \n"
@@ -21,9 +22,8 @@ public class Mochi {
 
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int n = 0;
-        String[] status = new String[100];
 
         while (true) {
             String command = scanner.nextLine(); // creates string so use .equals
@@ -34,8 +34,9 @@ public class Mochi {
             if (words[0].equals("mark")) {
                 System.out.println("Nice! I've marked this task as done:");
                 int target = Integer.parseInt(words[1]);
-                status[target - 1] = "[X] ";
-                System.out.println("  " + status[target - 1] + tasks[target - 1]);
+                Task t = tasks[target - 1];
+                t.mark();
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDescription());
                 System.out.println(separator);
                 continue;
             }
@@ -43,8 +44,9 @@ public class Mochi {
             if (words[0].equals("unmark")) {
                 System.out.println("OK, I've marked this task as not done yet:");
                 int target = Integer.parseInt(words[1]);
-                status[target - 1] = "[ ] ";
-                System.out.println("  " + status[target - 1] + tasks[target - 1]);
+                Task t = tasks[target - 1];
+                t.unmark();
+                System.out.println("  [" + t.getStatusIcon() + "] " + t.getDescription());
                 System.out.println(separator);
                 continue;
             }
@@ -56,20 +58,20 @@ public class Mochi {
             }
 
             if (command.equals("list")) {
-                System.out.println("Here are the tasks in your list: ");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < n; i++) {
-                    String listing = (i + 1) + "." + status[i] + tasks[i];
+                    String listing = (i + 1) + "." +
+                            "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription();
                     System.out.println(listing);
                 }
                 System.out.println(separator);
                 continue; // bc only exits when use says bye
             }
 
-            tasks[n] = command;
-            status[n] = "[ ] ";
-            n++;
+            tasks[n] = new Task(command);
 
-            System.out.println("added: " + command);
+            System.out.println("added: " + tasks[n].getDescription());
+            n++;
             System.out.println(separator);
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+/**
+ * Represents a task and whether it has been completed.
+ */
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+}
