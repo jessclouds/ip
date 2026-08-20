7c145c540196449216cec8252d66dd9f47b276df
jessica <e1669056@u.nus.edu>
2026-08-20 14:26:51 +0800

    Rename Duke to Mochi

---
+++ src/main/java/Mochi.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Mochi chatbot application.
+ */
+public class Mochi {
+    public static void main(String[] args) {
+        String banner = " __  __            _     _ \n"
+                + "|  \\/  | ___   ___| |__ (_)\n"
+                + "| |\\/| |/ _ \\ / __| '_ \\| |\n"
+                + "| |  | | (_) | (__| | | | |\n"
+                + "|_|  |_|\\___/ \\___|_| |_|_|\n";
+        System.out.println(banner);
+    }
+}
