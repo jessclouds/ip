f5541add575466f827039e5488ade89e6b671cf0
jessica <e1669056@u.nus.edu>
2026-08-20 14:52:37 +0800

    Commit Level-0

--- src/main/java/Mochi.java
+++ src/main/java/Mochi.java
@@ -8,6 +8,14 @@ public class Mochi {
                 + "| |\\/| |/ _ \\ / __| '_ \\| |\n"
                 + "| |  | | (_) | (__| | | | |\n"
                 + "|_|  |_|\\___/ \\___|_| |_|_|\n";
-        System.out.println(banner);
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.print(banner);
+        System.out.println("Hello! I'm Mochi.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
